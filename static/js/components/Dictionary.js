310d22371dc69fac9cb62fece6748160ae960029
Ubuntu <ubuntu@ip-172-31-3-147.us-west-2.compute.internal>
2022-04-06 04:51:09 +0000

    pretty much working

--- static/js/components/Dictionary.js
+++ static/js/components/Dictionary.js
@@ -17,108 +17,44 @@ export class Dictionary extends HTML {
     async addWord (name) {
         let response = await fetch(`https://dictionary.aurora.merxsolutions.com/AddWord?word_text=${name}`)
         let json = await response.json()
-        console.log(json)
-
+        return json
     }
 
-    async updateWord (id, name, definition) {
-        let response = await fetch("https://dictionary.aurora.merxsolutions.com/UpdateWord", {
-            params: {
-                "word_id": id,
-                "word_name": name,
-                "word_definition": definition
-            }
-        })
+    async addDefinition (id, definition) {
+        let response = await fetch(`https://dictionary.aurora.merxsolutions.com/AddDefinition?word_id=${id}&def_text=${definition}`)
         let json = await response.json()
-        this.words = this.words.map(word => word.id == id ? {id, name, definition} : word)
+        return json
     }
 
-    async DeleteWord (id) {
-        let response = await fetch("https://dictionary.aurora.merxsolutions.com/RemoveWord", {
-            params: {
-                "word_id": id
-            }
-        })
+    async updateWord (id, name) {
+        let response = await fetch(`https://dictionary.aurora.merxsolutions.com/UpdateWord?word_id=${id}&word_text=${name}`)
         let json = await response.json()
-        this.words = this.words.filter(word => word.id != id)
-        
+        return json
+    }
+
+    async updateDefinition (id, definition) {
+        let response = await fetch(`https://dictionary.aurora.merxsolutions.com/UpdateDefinition?def_id=${id}&def_text=${definition}`)
+        let json = await response.json()
+        return json
+    }
+
+    async removeDefinition (id) {
+        let response = await fetch(`https://dictionary.aurora.merxsolutions.com/RemoveDefinition?def_id=${id}`)
+        let json = await response.json()
+        return json
+    }
+
+    async removeWord (id) {
+        let response = await fetch(`https://dictionary.aurora.merxsolutions.com/RemoveWord?word_id=${id}`)
+        let json = await response.json()
+        return json
     }
 
     async getAllWordsWithDefinitions () {
-        // {
-        //     "object": "dictionary\\controllers\\cGetAllWordsWithDefinitions",
-        //     "data": [
-        //         {
-        //             "def_id": 1,
-        //             "word_id": 1,
-        //             "def_text": "fruit",
-        //             "OCDT": "2022-04-04 13:42:14",
-        //             "OMDT": "2022-04-04 19:42:14",
-        //             "OCU": 0,
-        //             "OMU": null,
-        //             "word": [
-        //                 {
-        //                     "word_id": 1,
-        //                     "word_text": "apple",
-        //                     "OCDT": "2022-04-02 12:02:42",
-        //                     "OMDT": "2022-04-02 18:02:42",
-        //                     "OCU": 0,
-        //                     "OMU": null
-        //                 }
-        //             ]
-        //         },
-        //         {
-        //             "def_id": 2,
-        //             "word_id": 1,
-        //             "def_text": "fruit",
-        //             "OCDT": "2022-04-04 13:49:35",
-        //             "OMDT": "2022-04-04 19:49:35",
-        //             "OCU": 0,
-        //             "OMU": null,
-        //             "word": [
-        //                 {
-        //                     "word_id": 1,
-        //                     "word_text": "apple",
-        //                     "OCDT": "2022-04-02 12:02:42",
-        //                     "OMDT": "2022-04-02 18:02:42",
-        //                     "OCU": 0,
-        //                     "OMU": null
-        //                 }
-        //             ]
-        //         },
-        //         {
-        //             "def_id": 3,
-        //             "word_id": 1,
-        //             "def_text": "fruit",
-        //             "OCDT": "2022-04-04 13:52:49",
-        //             "OMDT": "2022-04-04 19:52:49",
-        //             "OCU": 0,
-        //             "OMU": null,
-        //             "word": [
-        //                 {
-        //                     "word_id": 1,
-        //                     "word_text": "apple",
-        //                     "OCDT": "2022-04-02 12:02:42",
-        //                     "OMDT": "2022-04-02 18:02:42",
-        //                     "OCU": 0,
-        //                     "OMU": null
-        //                 }
-        //             ]
-        //         }
-        //     ],
-        //     "runtime": 11.221170425415039
-        // }
         let response = await fetch("https://dictionary.aurora.merxsolutions.com/GetAllWordsWithDefinitions")
         let json = await response.json()
 
-        //this.words = {'apple': {
-        //     word_text: 'apple',
-        //     word_id: 1,
-        //     def_list: [
-        //         {def_id: 1, def_text:'fruit'},
-        //         {def_id: 1, def_text:'fruit'}
-        //     ]
-        // }}
+        this.words = {}
         json['data'].forEach(item => {
             let {word_text, word_id} = item["word"][0]
             let {def_text, def_id} = item
@@ -149,32 +85,47 @@ export class Dictionary extends HTML {
             let word = this.words[key]
             let row = new HTML('tr').append(table)
             let name = new HTML('td', word['word_text']).append(row)
-            let definition = new HTML('td', word['def_list'].shift()['def_text']).append(row)
+            let def = word['def_list'].shift()
+            let definition = new HTML('td', '1. ' + def['def_text']).append(row)
             let action = new HTML('td').append(row)
-            let edit = new HTML('button', 'Edit', {click: function(){
-                alert("Hello")
-            }}).append(action)
-            let del = new HTML('button', 'Delete', {click: function(){
-                alert('Not sure why...')
+
+            let edit = new HTML('button', 'Edit', {click: () => {
+                this.updateWord(word['word_id'], prompt('Update word...')).then(() => {
+                    this.updateDefinition(def['def_id'], prompt('Update definition...')).then(() => this.render())
+                })
             }}).append(action)
 
-            word['def_list'].forEach(def => {
+            let addDef = new HTML('button', 'Add Definition', {click: () => {
+                this.addDefinition(word['word_id'], prompt('Enter another definition...')).then(response => {
+                    this.render()
+                })
+            }}).append(action)
+
+            let del = new HTML('button', 'Delete', {click: () => {
+                this.removeWord(word['word_id']).then(() => this.render())
+            }}).append(action)
+
+            word['def_list'].forEach((def, i) => {
                 let row = new HTML('tr').append(table)
                 let name = new HTML('td', '').append(row)
-                let definition = new HTML('td', def['def_text']).append(row)
+                let definition = new HTML('td', (i + 2) + '. ' + def['def_text']).append(row)
                 let action = new HTML('td').append(row)
-                let edit = new HTML('button', 'Edit', {click: function(){
-                    alert("Hello")
+
+                let edit = new HTML('button', 'Edit', {click: () => {
+                    this.updateDefinition(def['def_id'], prompt('Update definition...')).then(() => this.render())
                 }}).append(action)
-                let del = new HTML('button', 'Delete', {click: function(){
-                    alert('Not sure why...')
+
+                let del = new HTML('button', 'Delete', {click: () => {
+                    this.removeDefinition(def['def_id']).then(() => this.render())
                 }}).append(action)
             })
         }
         let add = new HTML('button', 'Add Word', {click: () => {
-            let word = prompt('Word')
-            let def = prompt('Definition')
-            this.addWord (word, def).then(r => this.render)
+            this.addWord(prompt('Enter a new word...')).then(response => {
+                this.addDefinition(response['data']['word_id'], prompt('Enter a new definition...')).then(response => {
+                    this.render()
+                })
+            })
         }}).append(this.el)
     }
 }
